0af310e0af71d78087a71c733b66f4603a5436d9
krishnatiwari005 <krishna.08072005@gmail.com>
2025-09-19 07:05:42 +0530

    added login and verify page

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,22 +1,11 @@
-apply plugin: 'com.android.application'
-apply plugin: 'kotlin-android'
-
-
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("kotlin-android") // ✅ use the proper kotlin plugin name
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
-    id("com.android.application")
-    
 }
 
 android {
-    defaultConfig {
-        multiDexEnabled true
-    }
-
     namespace = "com.example.authtask"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
@@ -31,21 +20,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.authtask"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion 21
-        targetSdk = flutter.targetSdkVersion 34
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-        multiDexEnabled true
-    }
+    applicationId = "com.example.authtask"
+    minSdk = flutter.minSdkVersion
+    targetSdk = flutter.targetSdkVersion
+    versionCode = flutter.versionCode
+    versionName = flutter.versionName
+}
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -54,9 +37,4 @@ android {
 flutter {
     source = "../.."
 }
-dependencies{
-    implementation(platform("com.google.firebase:firebase-bom:34.2.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("androidx.multidex:multidex:2.0.1")
 
-}
